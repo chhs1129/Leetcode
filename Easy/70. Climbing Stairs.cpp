530eb26f7a3153d3c9cf014cd9521a8ae6e4ac2e
Haosen Cheng <chhs1129@gmail.com>
2020-01-09 15:52:53 -0800

    Update 70. Climbing Stairs.cpp

--- Easy/70. Climbing Stairs.cpp
+++ Easy/70. Climbing Stairs.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int StepOne = 1;
-        int StepTwo = 0;
-        int ret = 0;
-        for(int i=0;i<n;i++)
-        {
-            ret = StepOne + StepTwo;
-            StepTwo = StepOne;
-            StepOne = ret;
-        }
-        return ret;
+        // dp[n] = dp[n - 1] + dp[n - 2]
+        //          1 steps     2 steps
+
+        if (n == 1)
+            return 1;
+        if (n < 1)
+            return 0;
+        
+        int dp[n];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (auto i = 2; i < n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[n - 1];
     }
 };
